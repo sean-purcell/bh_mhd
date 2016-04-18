3d323ad14912f477f4eb0be3dfb8c920d2910345
Sean Purcell <iburinoc@gmail.com>
2016-04-17 23:27:05 -0400

    setup initial_conditions shader

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,17 @@
 extern crate glium;
 extern crate rand;
 
-use std::cell::RefCell;
+mod simulate;
 
-use glium::{Program,VertexBuffer,IndexBuffer, Display};
-use glium::vertex::VertexBufferAny;
-use glium::index::{PrimitiveType,IndexBufferAny};
-use glium::backend::Facade;
-use glium::texture::Texture2d;
+use glium::Display;
 use glium::framebuffer::MultiOutputFrameBuffer;
+
+use simulate::{Data,Shaders};
 
 pub struct Sim<'a> {
     facade: &'a Display,
     data: Data,
-    sim_prog: Program,
-    cover_buffers: (VertexBufferAny,IndexBufferAny),
+    shaders: Shaders,
 }
 
 impl<'a> Sim<'a> {
@@ -23,38 +20,23 @@ impl<'a> Sim<'a> {
         Sim {
             facade: f,
             data: Data::new(f),
-            sim_prog: test_program(f),
-            cover_buffers: cover_buffers(f),
+            shaders: Shaders::new(f),
         }
     }
 
     pub fn test_render(&self) {
         use glium::{Surface,Rect,BlitTarget,uniforms};
-        let (vert_buf, ind_buf) = cover_buffers(self.facade);
 
         let layers = &self.data.layers;
 
         let tex1 = &layers.0.v_p;
-        let tex2 = &layers.0.b;
-        let mut fbo = {
-            let outputs = [("v_p", tex1), ("b", tex2)];
-            MultiOutputFrameBuffer::new(self.facade, outputs.iter().cloned()).unwrap()
-        };
-        let mut fbo1 = tex1.as_surface();
-        let mut fbo2 = tex2.as_surface();
-
-        fbo.draw(&vert_buf, &ind_buf, &self.sim_prog,
-            &uniforms::EmptyUniforms, &Default::default()).unwrap();
+        let fbo = tex1.as_surface();
 
         let target = self.facade.draw();
 
         let (s_width, s_height) = fbo.get_dimensions();
         let (t_width, t_height) = target.get_dimensions();
-        target.blit_from_simple_framebuffer(&fbo1,
-            &Rect {left: 0, bottom: 0, width: s_width, height: s_height},
-            &BlitTarget {left: 0, bottom: 0, width: t_width as i32, height: t_height as i32},
-            uniforms::MagnifySamplerFilter::Nearest);
-        target.blit_from_simple_framebuffer(&fbo1,
+        target.blit_from_simple_framebuffer(&fbo,
             &Rect {left: 0, bottom: 0, width: s_width, height: s_height},
             &BlitTarget {left: 0, bottom: 0, width: t_width as i32, height: t_height as i32},
             uniforms::MagnifySamplerFilter::Nearest);
@@ -62,136 +44,34 @@ impl<'a> Sim<'a> {
     }
 
     pub fn iteration(&self, dt: f32) {
-        use glium::{Surface,uniforms};
-        let front = self.data.front_layer();
-        let back = self.data.back_layer();
+        use glium::Surface;
+        use glium::uniforms::{MinifySamplerFilter,MagnifySamplerFilter};
+
+        let data = &self.data;
+        let front = data.front_layer();
+        let back = data.back_layer();
 
         let outputs = [("v_p", &front.v_p), ("b", &front.b)];
         let mut fbo = MultiOutputFrameBuffer::new(self.facade,
             outputs.iter().cloned()).unwrap();
 
-        let bufs = &self.cover_buffers;
-        fbo.draw(&bufs.0, &bufs.1, &self.sim_prog,
-            &uniforms::EmptyUniforms, &Default::default()).unwrap();
+        let uniforms = uniform! {
+            rads: data.dimensions.rads,
+            angs: data.dimensions.angs,
+            levs: data.dimensions.levs,
+            tex_v_p: back.v_p.sampled()
+                .minify_filter(MinifySamplerFilter::Nearest)
+                .magnify_filter(MagnifySamplerFilter::Nearest),
+            tex_b: back.b.sampled()
+                .minify_filter(MinifySamplerFilter::Nearest)
+                .magnify_filter(MagnifySamplerFilter::Nearest),
+        };/* here because returning Uniforms is very hard */
+
+        let bufs = &self.shaders.buffers;
+        fbo.draw(&bufs.0, &bufs.1, &self.shaders.initialize,
+            &uniforms, &Default::default()).unwrap();
 
         self.data.next_iter();
     }
-}
-
-struct Data {
-    layers: (Layer, Layer),
-    dimensions: SimDimensions,
-    iter: RefCell<i32>,
-}
-
-struct SimDimensions {
-    angs: u32,
-    rads: u32,
-    levs: u32,
-}
-
-impl Data {
-    fn new(f: &Display) -> Self {
-        let dims = {
-            let ang_num = 100u32;
-            let rad_num = 100u32;
-            let lev_num = 100u32;
-
-            SimDimensions {
-                angs: ang_num,
-                rads: rad_num,
-                levs: lev_num,
-            }
-        };
-
-        let layers = {
-            (Layer::new(f, dims.angs, dims.rads, dims.levs),
-            Layer::new(f, dims.angs, dims.rads, dims.levs))
-        };
-
-        Data { layers: layers,
-            dimensions: dims,
-            iter: RefCell::new(0) }
-    }
-
-    fn front_layer(&self) -> &Layer {
-        if *self.iter.borrow() % 2 == 0 {
-            &self.layers.0
-        } else {
-            &self.layers.1
-        }
-    }
-
-    fn back_layer(&self) -> &Layer {
-        if *self.iter.borrow() % 2 == 1 {
-            &self.layers.0
-        } else {
-            &self.layers.1
-        }
-    }
-
-    fn next_iter(&self) {
-        *self.iter.borrow_mut() += 1;
-    }
-}
-
-struct Layer {
-    v_p: Texture2d,
-    b: Texture2d,
-}
-
-impl Layer {
-    fn new(f: &Display, width: u32, height: u32, depth: u32) -> Self {
-        let format = glium::texture::UncompressedFloatFormat::F32F32F32F32;
-        let mipmaps = glium::texture::MipmapsOption::NoMipmap;
-
-        Layer {
-            v_p: Texture2d::empty_with_format(f,
-                format, mipmaps, width * depth, height).unwrap(),
-            b: Texture2d::empty_with_format(f,
-                format, mipmaps, width * depth, height).unwrap(),
-        }
-    }
-}
-
-fn test_program<F>(f: &F) -> Program where F: Facade {
-    let vert_str = r#"
-        #version 330
-        in vec2 pos;
-        out vec2 pos_o;
-        void main() {
-            pos_o = pos;
-            gl_Position = vec4(pos, 0.0, 1.0);
-        }"#;
-    let frag_str = r#"
-        #version 330
-        in vec2 pos_o;
-        layout(location = 0) out vec4 v_p;
-        layout(location = 1) out vec4 b;
-        void main() {
-            v_p = vec4((pos_o.x+1)/2, (pos_o.y+1)/2, 0, 1.0);
-            b = v_p.grba;
-        }"#;
-
-    Program::from_source(f, vert_str, frag_str, None).unwrap()
-}
-
-fn cover_buffers<F>(f: &F) -> (VertexBufferAny, IndexBufferAny)
-    where F: Facade {
-
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        pos: (f32, f32),
-    }
-
-    implement_vertex!(Vertex, pos);
-
-    (
-        VertexBuffer::new(f, &[
-            Vertex { pos: (-1.0,  1.0) }, Vertex { pos: (1.0,  1.0) },
-            Vertex { pos: (-1.0, -1.0) }, Vertex { pos: (1.0, -1.0) },
-        ]).unwrap().into(),
-        IndexBuffer::new(f, PrimitiveType::TriangleStrip, &[0u8, 1, 2, 3]).unwrap().into()
-    )
 }
 
--- src/simulate.rs
+++ src/simulate.rs
@@ -1,27 +1,169 @@
 extern crate glium;
 
-use glium::Program;
+use std::cell::RefCell;
 
-const PREAMBLE: &'static str = r#"
-    #version 330
-    uniform uint rads; /* texture height */
-    uniform uint angs; /* texture width per section */
-    uniform uint levs; /* texture sections
+use glium::{Program,VertexBuffer,IndexBuffer,Display};
+use glium::backend::Facade;
+use glium::vertex::VertexBufferAny;
+use glium::index::{PrimitiveType,IndexBufferAny};
+use glium::texture::Texture2d;
 
-    in vec2 pos;
-"#;
+pub struct Data {
+    pub layers: (Layer, Layer),
+    pub dimensions: SimDimensions,
+    pub iter: RefCell<i32>,
+}
+
+pub struct SimDimensions {
+    pub angs: u32,
+    pub rads: u32,
+    pub levs: u32,
+}
+
+impl Data {
+    pub fn new(f: &Display) -> Self {
+        let dims = {
+            let ang_num = 100u32;
+            let rad_num = 100u32;
+            let lev_num = 100u32;
+
+            SimDimensions {
+                angs: ang_num,
+                rads: rad_num,
+                levs: lev_num,
+            }
+        };
+
+        let layers = {
+            (Layer::new(f, dims.angs, dims.rads, dims.levs),
+            Layer::new(f, dims.angs, dims.rads, dims.levs))
+        };
+
+        Data { layers: layers,
+            dimensions: dims,
+            iter: RefCell::new(0) }
+    }
+
+    pub fn front_layer(&self) -> &Layer {
+        if *self.iter.borrow() % 2 == 0 {
+            &self.layers.0
+        } else {
+            &self.layers.1
+        }
+    }
+
+    pub fn back_layer(&self) -> &Layer {
+        if *self.iter.borrow() % 2 == 1 {
+            &self.layers.0
+        } else {
+            &self.layers.1
+        }
+    }
+
+    pub fn next_iter(&self) {
+        *self.iter.borrow_mut() += 1;
+    }
+}
+
+pub struct Layer {
+    pub v_p: Texture2d,
+    pub b: Texture2d,
+}
+
+impl Layer {
+    fn new(f: &Display, width: u32, height: u32, depth: u32) -> Self {
+        let format = glium::texture::UncompressedFloatFormat::F32F32F32F32;
+        let mipmaps = glium::texture::MipmapsOption::NoMipmap;
+
+        Layer {
+            v_p: Texture2d::empty_with_format(f,
+                format, mipmaps, width * depth, height).unwrap(),
+            b: Texture2d::empty_with_format(f,
+                format, mipmaps, width * depth, height).unwrap(),
+        }
+    }
+}
 
 pub struct Shaders {
     pub initialize: Program,
+    pub buffers: (VertexBufferAny, IndexBufferAny),
 }
 
 impl Shaders {
-    pub fn new() {
-        format!(r#"
-        {preamble}
-        void main() {
-
-        }"#);
+    pub fn new(f: &Display) -> Self {
+        Shaders {
+            initialize: initial_conditions::program(f),
+            buffers: cover_buffers(f),
+        }
     }
 }
+
+fn cover_buffers<F>(f: &F) -> (VertexBufferAny, IndexBufferAny)
+    where F: Facade {
+
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        uv: (f32, f32),
+    }
+
+    implement_vertex!(Vertex, uv);
+
+    (
+        VertexBuffer::new(f, &[
+            Vertex { uv: (-1.0,  1.0) }, Vertex { uv: (1.0,  1.0) },
+            Vertex { uv: (-1.0, -1.0) }, Vertex { uv: (1.0, -1.0) },
+        ]).unwrap().into(),
+        IndexBuffer::new(f, PrimitiveType::TriangleStrip, &[0u8, 1, 2, 3]).unwrap().into()
+    )
+}
+
+mod initial_conditions {
+    use glium::{Display,Program};
+    pub fn program(f: &Display) -> Program {
+        Program::from_source(f,
+            ::simulate::VERT_SHADER,
+            &frag_shader(),
+            None).unwrap()
+    }
+
+    fn frag_shader() -> String {
+        format!(r#"
+
+        {preamble}
+
+        void main() {{
+            /* for testing purposes */
+            float x = fract(pos.x * levs * angs + 0.5);
+            float y = fract(pos.y * rads + 0.5);
+            v_p = b = vec4(x, y, 0.0, 1.0);
+        }}"#,
+        preamble = ::simulate::FRAG_PREAMBLE)
+    }
+}
+
+const VERT_SHADER: &'static str = r#"
+    #version 330
+    in vec2 uv;
+    out vec2 pos;
+
+    void main() {
+        gl_Position = vec4(uv, 0.0, 1.0);
+        pos = uv;
+    }
+"#;
+
+const FRAG_PREAMBLE: &'static str = r#"
+    #version 330
+    uniform uint rads; /* texture height */
+    uniform uint angs; /* texture width per section */
+    uniform uint levs; /* texture section count */
+
+    in vec2 pos;
+
+    uniform sampler2D tex_v_p;
+    uniform sampler2D tex_b;
+
+    layout(location = 0) out vec4 v_p;
+    layout(location = 1) out vec4 b;
+"#;
 
